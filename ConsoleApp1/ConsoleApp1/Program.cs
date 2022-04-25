c012a387322427c610cac463f1fe4006f0ae9758
Tatiana Komarova <tat.komaro2002@yandex.ru>
2022-04-25 18:21:05 +0700

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson2;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please, enter the name");
+        var name = Console.ReadLine();
+
+        Console.WriteLine($"Hello word {name}" );
+
+        Console.WriteLine("Please, enter the age");
+        var age = Console.ReadLine();
+        var myLetter = "Мне " + age + " лет";
+        Console.WriteLine(myLetter);
+        Console.Read();
+    }
+}
